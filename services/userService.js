2c22e5d5c159c0f75eb59ab683732e59f9bd9f23
Mariano Inostroza <mariano.f.inostroza@gmail.com>
2026-04-01 17:52:37 -0300

    se corrigen bugs de conexión con db

--- services/userService.js
+++ services/userService.js
@@ -1,6 +1,6 @@
 import pool from '../db/db.js';
 
 export const getAllUsers = async () => {
-    const result = await pool.query('SELECT id_user, nombre FROM usuarios');
+    const result = await pool.query('SELECT id_user, nombre FROM users');
     return result.rows;
 };
